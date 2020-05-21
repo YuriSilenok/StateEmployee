a533f81f45eeda3e496eef7892720bd7d3ec060e
Юрий Силенок <yuri.silenok@gmail.com>
2020-05-21 15:21:42 +0300

    добавил кодировку и проверку вехи, ответсвенного

--- Terminal/gh.py
+++ Terminal/gh.py
@@ -1,12 +1,14 @@
+#!/usr/bin/python3.8
+# coding=UTF-8
+
 from github import Github
-import vk
 from datetime import datetime
 
 USERNAME = 'USERNAME'
 PASSWORD = 'PASSWORD'
 ORG = 'Students-of-the-city-of-Kostroma'
 REPO = 'trpo_automation'
-TEAMLEADS = ['Svyat935','urec-programmec']
+TEAMLEADS = ['Svyat935','urec-programmec', 'avilova']
 
 githib = Github(USERNAME, PASSWORD)
 org = githib.get_organization(ORG)
@@ -22,11 +24,20 @@ for branch in repo.get_branches():
     if del_branch:
         del_branch_list.append(branch.name)
 if del_branch_list:
-    issue = repo.create_issue(title='Нарушено правило именования веток', body=str(del_branch_list))
+    print('Нарушено правило именования веток', del_branch_list)
+    issue = repo.create_issue(title='Нарушено правило именования веток',body=str(del_branch_list))
     issue.add_to_assignees(TEAMLEADS)
 
-for issue in repo.get_issues():
+for issue in repo.get_issues(state='open'):
     if issue.state == 'open' and (datetime.now() - issue.updated_at).days >= 7:
+        print('Какие есть обновления по этой задаче?', issue)
         issue.create_comment('Какие есть обновления по этой задаче?')
     if issue.body == '':
+        print('Пожалуйста, добавьте описание к этой задаче', issue)
         issue.create_comment('Пожалуйста, добавьте описание к этой задаче')
+    if issue.milestone is None:
+        print('Пожалуйста, добавьте веху к этой задаче', issue)
+        issue.create_comment('Пожалуйста, добавьте веху к этой задаче')
+    if issue.assignee is None:
+        print('Пожалуйста, добавьте ответсвенного на эту задачу', issue)
+        issue.create_comment('Пожалуйста, добавьте ответсвенного на эту задачу')
