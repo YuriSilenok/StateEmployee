e39eac9e66c6e08e25b13704f41d63a38ed55f23
Юрий Силенок <yuri.silenok@gmail.com>
2020-05-21 13:36:55 +0300

    Добавил проверку старых задач

--- Terminal/gh.py
+++ Terminal/gh.py
@@ -1,10 +1,12 @@
 from github import Github
 import vk
+from datetime import datetime
 
 USERNAME = 'USERNAME'
 PASSWORD = 'PASSWORD'
 ORG = 'Students-of-the-city-of-Kostroma'
 REPO = 'trpo_automation'
+TEAMLEADS = ['Svyat935','urec-programmec']
 
 githib = Github(USERNAME, PASSWORD)
 org = githib.get_organization(ORG)
@@ -20,8 +22,11 @@ for branch in repo.get_branches():
     if del_branch:
         del_branch_list.append(branch.name)
 if del_branch_list:
-    repo.create_issue(title="Нарушено правило именования веток",body=str(del_branch_list))
+    issue = repo.create_issue(title="Нарушено правило именования веток",body=str(del_branch_list))
+    issue.add_to_assignees(TEAMLEADS)
 
 for issue in repo.get_issues():
+    if issue.state == 'open' and (datetime.now() - issue.updated_at).days >= 7:
+        issue.create_comment('Какие есть обновления по этой задаче?')
     if issue.body == '':
         issue.create_comment('Пожалуйста, добавьте описание к этой задаче')
